721deef2ce0c6f9a9b52c962800eef37a40d1665
Sinyuuuu <144888565+Sinyuuuu@users.noreply.github.com>
2023-09-20 16:17:15 +0800

    Add files via upload

---
+++ 1111134010/+-乘除/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    int NO1 = 9;
+    int No2 = 6;
+    int sum, cha, ji, shan,a = 0;
+    sum = (NO1 + No2);
+    cha = (NO1 - No2);
+    ji = (NO1 * No2); 
+    shan = (NO1 / No2);
+    a= (NO1 % No2);
+    printf("和=%d\n", sum);
+    printf("差=%d\n",cha);
+    printf("積=%d\n", ji);
+    printf("商=%d\n", shan);
+    printf("餘數=%d\n", a);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
