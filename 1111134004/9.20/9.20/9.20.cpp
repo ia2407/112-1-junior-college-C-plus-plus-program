98ab4aa13390609d191c800006fb346c465067c1
tingyu0215 <144888785+tingyu0215@users.noreply.github.com>
2023-09-21 09:08:18 +0800

    Add files via upload

---
+++ 1111134004/9.20/9.20/9.20.cpp
@@ -0,0 +1,32 @@
+﻿// 9.20.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+int main()
+{
+    int num1, num2, add, sub, mult, div,re;
+    num1 = 5;
+    num2 = 5;
+    add = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+    re = num1 % num2;
+    printf("%d和%d的和為%d\n", num1, num2, add);
+    printf("%d和%d的差為%d\n", num1, num2, sub);
+    printf("%d和%d的積為%d\n", num1, num2, mult);
+    printf("%d和%d的商為%d\n", num1, num2, div);
+    printf("%d和%d的餘數為%d\n", num1, num2, re);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
