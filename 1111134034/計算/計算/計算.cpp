f2c589f3506d76b17399ef8c3fac08be216441d6
yihuang1022 <144888696+yihuang1022@users.noreply.github.com>
2023-10-11 16:58:29 +0800

    Add files via upload

---
+++ 1111134034/計算/計算/計算.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+int main(void)
+{
+    double aim = 0;
+    double amount = 0;
+    double principal = 0.0;
+    double rate = 0.00;
+    int year = 0;
+    std::cout << "輸入目標的錢";
+
+    std::cin >> aim;
+    std::cout << "輸入本金";
+
+    std::cin >> principal;
+    std::cout << "輸入利率";
+
+    std::cin >> rate;
+
+    while (amount < aim)
+    {
+        year = year + 1;
+        amount = principal * pow(1 + rate, year);
+        std::cout << "第" << year << "年" << "    " << "總計" << amount << "    " << "目標" << aim << "\n";
+
+    }
+}
