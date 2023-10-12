ddfc6a5e2960ba93567df2ddfc91154596db700e
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2023-10-12 23:18:42 +0800

    Add files via upload

---
+++ 1111134045/年利率/年利率/年利率.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+int goal,year=0;
+double principal = 1000.0;
+double a= 1.05;
+int main()
+{
+	printf("請輸入目標:");
+	scanf_s("%d" , &goal);
+	while (principal<goal)
+	{
+		principal = principal * a;
+		year++;
+	}
+	printf("所需年限是:%d",year);
+}
+
+
