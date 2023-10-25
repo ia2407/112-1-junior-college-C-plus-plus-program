fd3ef125d3b385e146917a5580253bd846884915
Rodtls <144888506+Rodtls@users.noreply.github.com>
2023-10-25 16:08:35 +0800

    Add files via upload

---
+++ 1111134012/1025/印星星/ConsoleApplication4/副程式印星星.cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+
+char one();
+char two();
+char thr();
+char fou();
+char one(int a) {
+	for (int c = 1; c <= a; c++) {
+		for (int b = 1; b <= c; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+char two(int a) {
+	for (int c = 1; c <= a; c++) {
+		for (int b = c; b <= a; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+char thr(int a) {
+	for (int t = 1; t <= a; t++) {
+		for (int b = 1; b <= a - t; b++) {
+			printf(" ");
+		}
+		for (int c = 1; c <= t; c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+char fou(int a) {
+	for (int r = 0; r <= a; r++) {
+		for (int b = 1; b <= r; b++) {
+			printf(" ");
+		}
+		for (int c = 1; c <= a - r; c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+int main()
+{
+	int i;
+	printf("輸入數值");
+	scanf_s("%d", &i);
+	one(i);
+
+	int a;
+	printf("輸入數值");
+	scanf_s("%d", &a);
+	two(a);
+
+	int f;
+	printf("輸入數值");
+	scanf_s("%d", &f);
+	thr(f);
+
+	int q;
+	printf("輸入數值");
+	scanf_s("%d", &q);
+	fou(q);
+}
+
---
+++ 1111134012/1025/算pi/ConsoleApplication4/算pi.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+double one();
+
+double one(double a) {
+	double result = 4.0;
+	for (int i = 2; i <= a; i++) {//計數變數初始化;條件判斷;遞增
+		printf("%lf\n", result);
+		if (!(i % 2))//imod2餘數=0的話re=0(偶數)餘數=1re=1(奇數)
+			result -= 4.0 / (i * 2 - 1);//(re=1)
+		else result += 4.0 / (i * 2 - 1);//(re=0)
+
+	}printf("%lf\n", result);
+	return 0;
+}
+
+int main()
+{
+	int value ;
+	printf("please input the number of terms:");
+	scanf_s("%d", &value);//&是用來取得變數value的記憶體位址
+	one(value);
+}
+
+
