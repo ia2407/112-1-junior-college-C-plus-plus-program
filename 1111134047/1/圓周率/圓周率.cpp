9816e01efe42335a0e56230d5b16f0e5886d7caf
chengyu0116 <144889049+chengyu0116@users.noreply.github.com>
2023-10-25 16:39:10 +0800

    Add files via upload

---
+++ 1111134047/1/圓周率/圓周率.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+
+int play(int times)
+{
+	int hehe;
+	double a, b, d, e = 4;
+	for (a = 2; a <= times; a++)
+	{
+		d = e;
+		b = (a * 2) - 1;
+		hehe = a;
+		if (hehe % 2 == 0)
+		{
+			e = d - (4 / b);
+		}
+		else
+		{
+			e = d + (4 / b);
+		}
+		printf("%lf\n", e);
+	}
+	return e;
+}
+int main()
+{
+	int a;
+	printf("請輸入次數:");
+	scanf_s("%d",&a);
+	play(a);
+}
+
+
