1f9ab206d1e89a50f9d06ce95a6ba77d553ceca8
Benny0820 <144888599+Benny0820@users.noreply.github.com>
2023-10-27 00:54:23 +0800

    Add files via upload

---
+++ 1111134046/第7周/第一題/第一題.cpp
@@ -0,0 +1,80 @@
+﻿// 第一題.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+
+
+int first(int a)
+{
+	int i = 1, j = 1, k = 1;
+	for (i = 1; i <= a; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
+int second(int a)
+{
+	int i = 1, j = 1, k = 1;
+	for (i = a; i >= 1; i--)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
+int third(int a)
+{
+	int i = 1, j = 1, k = 1;
+	for (i = 1; i <= a; i++)
+	{
+		for (j = 1; j <= a - i; j++)
+		{
+			printf(" ");
+		}
+		for (k = 1; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	printf("\n");
+	return 0;
+}
+int forth(int a)
+{
+	int i = 1, j = 1, k = 1;
+	for (i = a; i >= 1; i--)
+	{
+		for (j = 1; j <= a - i; j++)
+		{
+			printf(" ");
+		}
+		for (k = 1; k <= i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+int main()
+{
+	int h = 0;
+	printf("輸入高\n");
+	scanf_s("%d", &h);
+	first(h);
+	second(h);
+	third(h);
+	forth(h);
+}
---
+++ 1111134046/第7周/第二題/第二題.cpp
@@ -0,0 +1,25 @@
+﻿// 第二題.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+
+float abcd(int x)
+{   float pi = 0, a = 1; 
+    int c = 1;
+    for (int i = 1; i <= x; i++)
+    {         
+     pi += 4 / a * c;
+     a += 2;
+     c = c * -1; 
+    }    
+     return pi;
+}
+int main()
+{   
+    int b = 0;
+    scanf_s("%d", &b);  
+    printf("\n%f", abcd(b));
+}
+
+
