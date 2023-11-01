2194a938f1c2e2bee43e493e1d293c686fa32f51
jrfu0908 <144890122+jrfu0908@users.noreply.github.com>
2023-11-01 16:54:57 +0800

    Add files via upload

---
+++ 1111134043/111/111-2.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+int fact(int x)
+{
+    int a = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        a *= i;
+    }
+    return a;
+}
+
+double exponen(int x)
+{
+    double a = 1;
+    double result = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        a *= i;
+        result += 1.0 / a;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int a;
+    double result;
+    scanf_s("%d", &a);
+    result = exponen(a);
+    printf("%lf", result);
+}
