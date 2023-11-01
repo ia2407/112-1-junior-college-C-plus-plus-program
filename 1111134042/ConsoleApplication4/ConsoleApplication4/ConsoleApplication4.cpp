efe38d0ea7dec1c97a5585401b12298ed47077a2
Hsuan07531 <144888928+Hsuan07531@users.noreply.github.com>
2023-11-01 16:23:32 +0800

    Add files via upload

--- 1111134042/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ 1111134042/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,30 +1,50 @@
 ﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <iostream>
+#include <math.h>
 #include <stdio.h>
-int main()
-{int score = 0;
-printf("請輸入學生成績:");
-	scanf_s("%d", &score);
-	if (score >= 90)
-		printf("表現優秀!得A");
-	else if (score >= 80)
-		printf("表顯不錯!得B");
-	else if (score >= 70)
-		printf("恭喜通過!得C");
-	else if (score >= 60)
-		printf("好險有過!得D");
-	else printf("請於8/31前報名暑修!");
+
+double exponen(int z);
+int fact(int x);
+double exponen_x(int z, int x);
+
+
+double exponen(int z) {
+    int count2 = 1;
+    double exp = 1.0;
+    for (count2 = 1; count2 <= z; count2++) {
+        exp = (1 / fact(count2));
+    }
+    return exp;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+int fact(int x) {
+    int count = 1;
+    int result = 1;
+    for (count = 1; count <= x; count++) {
+        result = (result * x);
+    }
+    return result;
+}
+double exponen_x(int z, int x) {
+    int count3 = 1;
+    double exp2 = 1.0;
+    for (count3 = 1; count3 <= z; count3++) {
+        exp2 = (exp2 + ((pow(x, count3)) + fact(count3)));
+    }
+    return exp2;
+}
+int main() {
+    int myVar = 1;
+    int z = 1;
+    int x = 1;
+    printf("請輸入一個整數");
+    scanf_s("%d", &myVar);
+    printf("你總共要執行幾項\?");
+    scanf_s("%d", &z);
+    printf("第三題EXP的次方值\?");
+    scanf_s("%d", &x);
+    printf("%d\n", fact(myVar));
+    printf("%f\n", exponen(z));
+    printf("%f\n", exponen_x(z, x));
+    return 0;
+}
