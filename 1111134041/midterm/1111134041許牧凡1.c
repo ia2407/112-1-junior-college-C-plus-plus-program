336d7724dea044598173031dff8c95f50201c34e
ryan995ryan995 <144889397+ryan995ryan995@users.noreply.github.com>
2023-11-08 15:34:51 +0800

    Add files via upload

---
+++ 1111134041/midterm/1111134041許牧凡1.c
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int p = 0;
+    double r = 0;
+    int aims= 0 ;
+    int m = p * r;
+    int n = 0;
+    
+    
+    int reward();
+
+    printf("fu/請輸入本金\n");
+    scanf_s("%d", & p);
+    
+    printf("fu/請輸入年利率\n");
+    scanf_s("%f", & r);
+   
+    printf("fu/請輸入存款目標\n");
+
+   scanf_s("%d", &aims);
+
+    return 0;
+
+    while (m < aims);
+    {
+        m = m + m;
+        n = n + 1;
+    }
+    printf("還需要%d年才會達成存款目標\n", n)
+
+
+
+
+
+
+
+
+        ;
+}
