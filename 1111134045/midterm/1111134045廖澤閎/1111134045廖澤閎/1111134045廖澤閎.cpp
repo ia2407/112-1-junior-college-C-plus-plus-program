e6fc0cdbd325a48b58c1f88adce627fe29db7813
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2023-11-08 15:32:47 +0800

    Add files via upload

---
+++ 1111134045/midterm/1111134045廖澤閎/1111134045廖澤閎/1111134045廖澤閎.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+int reward(double p, double r, double aims)
+{
+    int years=0;
+    double moneyrate;
+    moneyrate = p * r;
+    for (years=0; p<aims; years++)
+    {
+        p = p + moneyrate;
+    }
+    return(years);
+}
+int main()
+{
+    double money, rate, aims;
+    printf("請輸入本金:");
+    scanf_s("%lf", &money);
+    printf("請輸入利率:");
+    scanf_s("%lf", &rate);
+    printf("請輸入目標:");
+    scanf_s("%lf", &aims);
+    printf("所需年限為:%d年", reward(money,rate,aims));
+}
+
