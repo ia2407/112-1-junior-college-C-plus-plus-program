b0e18dd9d5cc180b5eecf014655b1ee65f6ad435
cheseees <144889454+cheseees@users.noreply.github.com>
2023-11-22 15:58:01 +0800

    Add files via upload

---
+++ 1111134022/1111134022-2/11 22 34022.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <stdio.h>
+#define RESPONSES_SIZE 40
+#define FREQUENCY_SIZE 11
+
+void print_result(int frequency[FREQUENCY_SIZE])
+{
+    printf("%s%17s\t\t%s\n", "Rating", "Frequency", "his");
+
+    for (int rating = 1; rating < FREQUENCY_SIZE; ++rating) {
+        printf("%6d%17d\t\t", rating, frequency[rating]);
+
+        for (int i = 1; i <= frequency[rating]; ++i)
+        {
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int frequency[FREQUENCY_SIZE] = { 0 };
+
+    int responses[RESPONSES_SIZE] = { 1, 2, 6, 4, 8, 5, 9, 7, 8, 10,
+         1, 6, 3, 8, 6, 10, 3, 8, 2, 7, 6, 5, 7, 6, 8, 6, 7, 5, 6, 6,
+         5, 6, 7, 5, 6, 4, 8, 6, 8, 10 };
+
+    for (int answer = 0; answer < RESPONSES_SIZE; ++answer)
+    {
+        ++frequency[responses[answer]];
+    }
+
+    print_result(frequency);
+}
+
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
