06afb08cc5eaa499c29ae483047f64ea33c563f5
毛澤東 <74415270+kellenjou@users.noreply.github.com>
2023-11-29 15:44:09 +0800

    Add files via upload

---
+++ 1111134037/11月29日的作業/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,75 @@
+﻿// Fig. 6.15: fig06_15.c
+// Sorting an array's values into ascending order.
+#include <stdio.h>
+#define SIZE 11
+#include <iostream>
+
+
+// function main begins program execution
+int main(void)
+{
+    // initialize a
+    int sis = 0;
+    int tim = 10;
+    int a[SIZE] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    for (int o = 0; o <= 9; --tim)
+    {
+        o = o + 1;
+        std::cout << "輸入" << tim << "個整數";
+        
+        std::cin >> sis;
+        a[o] = sis;
+
+    }
+    
+    
+
+    puts("Data items in original order");
+
+    // output original array
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+
+    // bubble sort                                         
+    // loop to control number of passes                    
+    for (unsigned int pass = 1; pass < SIZE; ++pass) {
+
+        // loop to control number of comparisons per pass   
+        for (size_t i = 0; i < SIZE - 1; ++i) {
+
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (a[i] > a[i + 1]) {
+                int hold = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = hold;
+            }
+        }
+    }
+
+    puts("\nData items in ascending order");
+
+    // output sorted array
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+
+}
+
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
