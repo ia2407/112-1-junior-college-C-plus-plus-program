635b65e2780f5a76c8bf26aec7192b32ad89f1b0
Hsuan07531 <144888928+Hsuan07531@users.noreply.github.com>
2023-11-29 15:44:41 +0800

    Add files via upload

--- 1111134042/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ 1111134042/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,50 +1,66 @@
 ﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <math.h>
-#include <stdio.h>
-
-double exponen(int z);
-int fact(int x);
-double exponen_x(int z, int x);
+#include <iostream>
 
 
-double exponen(int z) {
-    int count2 = 1;
-    double exp = 1.0;
-    for (count2 = 1; count2 <= z; count2++) {
-        exp = (1 / fact(count2));
+#define SIZE 10
+void bubble(int arr[SIZE], unsigned int size);
+// function main begins program execution
+int main(void)
+{
+    // initialize a
+    int a[SIZE], size, i; //= { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+
+    printf("你要輸入幾個值");
+    scanf_s("%d", &size);
+    for (i = 0; i < size; i++)
+        scanf_s("%d", &a[i]);
+
+    puts("Data items in original order");
+
+    // output original array
+    for (size_t i = 0; i < size; ++i) {
+        printf("%4d", a[i]);
     }
-    return exp;
-}
-int fact(int x) {
-    int count = 1;
-    int result = 1;
-    for (count = 1; count <= x; count++) {
-        result = (result * x);
+
+    // bubble sort                                         
+    // loop to control number of passes                    
+    bubble(a, size);
+
+    puts("\nData items in ascending order");
+
+    // output sorted array
+    for (size_t i = 0; i < size; ++i) {
+        printf("%4d", a[i]);
     }
-    return result;
+
+    puts("");
 }
-double exponen_x(int z, int x) {
-    int count3 = 1;
-    double exp2 = 1.0;
-    for (count3 = 1; count3 <= z; count3++) {
-        exp2 = (exp2 + ((pow(x, count3)) + fact(count3)));
+
+
+
+void bubble(int arr[SIZE], unsigned int size)
+{
+    for (unsigned int pass = 1; pass < size; ++pass) {
+
+        // loop to control number of comparisons per pass   
+        for (size_t i = 0; i < size - 1; ++i) {
+
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (arr[i] > arr[i + 1]) {
+                int hold = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = hold;
+            }
+        }
     }
-    return exp2;
 }
-int main() {
-    int myVar = 1;
-    int z = 1;
-    int x = 1;
-    printf("請輸入一個整數");
-    scanf_s("%d", &myVar);
-    printf("你總共要執行幾項\?");
-    scanf_s("%d", &z);
-    printf("第三題EXP的次方值\?");
-    scanf_s("%d", &x);
-    printf("%d\n", fact(myVar));
-    printf("%f\n", exponen(z));
-    printf("%f\n", exponen_x(z, x));
-    return 0;
-}
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
