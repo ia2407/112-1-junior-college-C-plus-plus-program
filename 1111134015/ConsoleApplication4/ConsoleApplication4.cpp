48a8d475ac5b66deed105275b3545111e54a797f
張斯婷 <144889000+zst07@users.noreply.github.com>
2023-11-29 15:46:29 +0800

    Add files via upload

--- 1111134015/ConsoleApplication4/ConsoleApplication4.cpp
+++ 1111134015/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,44 +1,56 @@
-﻿// Fig. 6.15: fig06_15.c
-// Sorting an array's values into ascending order.
-#include <stdio.h>
-#define SIZE 10
+﻿#include <iostream>
 
+
+#define SIZE 10
+void bubble(int arr[SIZE], unsigned int size);
 // function main begins program execution
 int main(void)
 {
     // initialize a
-    int a[SIZE] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+    int a[SIZE], size, i; //= { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+
+    printf("你要輸入幾個值");
+    scanf_s("%d", &size);
+    for (i = 0; i < size; i++)
+        scanf_s("%d", &a[i]);
 
     puts("Data items in original order");
 
     // output original array
-    for (size_t i = 0; i < SIZE; ++i) {
+    for (size_t i = 0; i < size; ++i) {
         printf("%4d", a[i]);
     }
 
     // bubble sort                                         
     // loop to control number of passes                    
-    for (unsigned int pass = 1; pass < SIZE; ++pass) {
-
-        // loop to control number of comparisons per pass   
-        for (size_t i = 0; i < SIZE - 1; ++i) {
-
-            // compare adjacent elements and swap them if first 
-            // element is greater than second element           
-            if (a[i] > a[i + 1]) {
-                int hold = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = hold;
-            }
-        }
-    }
+    bubble(a, size);
 
     puts("\nData items in ascending order");
 
     // output sorted array
-    for (size_t i = 0; i < SIZE; ++i) {
+    for (size_t i = 0; i < size; ++i) {
         printf("%4d", a[i]);
     }
 
     puts("");
+}
+
+
+
+void bubble(int arr[SIZE], unsigned int size)
+{
+    for (unsigned int pass = 1; pass < size; ++pass) {
+
+        // loop to control number of comparisons per pass   
+        for (size_t i = 0; i < size - 1; ++i) {
+
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (arr[i] > arr[i + 1]) {
+                int hold = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = hold;
+            }
+        }
+    }
 }
