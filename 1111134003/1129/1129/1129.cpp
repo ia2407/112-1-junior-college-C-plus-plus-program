362a0605d0e04df29f294720af5f8c9411817169
wanpinggg <144889474+wanpinggg@users.noreply.github.com>
2023-11-29 16:05:33 +0800

    Add files via upload

---
+++ 1111134003/1129/1129/1129.cpp
@@ -0,0 +1,62 @@
+﻿// 1129.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#define SIZE 10
+
+void bubble(int ar[SIZE], int size);
+
+int main(void)
+{
+    // initialize a
+    int a[SIZE] = {}, size, i;
+
+    printf("要輸入幾個值(不可超過10個)\n");
+    scanf_s("%d", &size);
+
+    printf("請輸入%d個值\n",size);
+
+    for (i = 0; i < size; i++)
+    {
+        scanf_s("%d", &a[i]);
+    }
+
+    printf("原本的數字排列\n");
+    for ( i = 0; i < size; ++i)
+    {
+        printf("%4d", a[i]);
+    }
+    printf("\n");
+    bubble(a,size);
+
+}
+
+void bubble(int arr[SIZE], int size)
+{
+    for (int pass = 1; pass < size; ++pass) {
+
+        for (int i = 0; i < size - 1; ++i) {
+        
+            if (arr[i] > arr[i + 1]) {
+                int hold = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = hold;
+            }
+        }
+    }
+    printf("目前的數字排列\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%4d", arr [i]);
+    }
+    puts("");
+}
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
