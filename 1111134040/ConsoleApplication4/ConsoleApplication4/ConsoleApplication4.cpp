892f7394fd85cf98cf5add289ebea5df45969549
voguevity <144888849+voguevity@users.noreply.github.com>
2023-12-06 16:06:09 +0800

    Add files via upload

--- 1111134040/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ 1111134040/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,50 +1,73 @@
 ﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <math.h>
 #include <stdio.h>
 
-double exponen(int z);
-int fact(int x);
-double exponen_x(int z, int x);
+// 排序副程式
+void bubbleSort(int SIZE[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (SIZE[j] > SIZE[j + 1]) {
+                // 交換元素
+                int temp = SIZE[j];
+                SIZE[j] = SIZE[j + 1];
+                SIZE[j + 1] = temp;
+            }
+        }
+    }
+}
 
+// 二元搜尋副程式
+int binarySearch(int SIZE[], int low, int high, int key) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-double exponen(int z) {
-    int count2 = 1;
-    double exp = 1.0;
-    for (count2 = 1; count2 <= z; count2++) {
-        exp = (1 / fact(count2));
+        if (SIZE[mid] == key)
+            return mid;
+
+        if (SIZE[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
     }
-    return exp;
+
+    return -1; // 找不到
 }
-int fact(int x) {
-    int count = 1;
-    int result = 1;
-    for (count = 1; count <= x; count++) {
-        result = (result + x);
-    }
-    return result;
-}
-double exponen_x(int z, int x) {
-    int count3 = 1;
-    double exp2 = 1.0;
-    for (count3 = 1; count3 <= z; count3++) {
-        exp2 = (exp2 / ((pow(x, count3)) / fact(count3)));
-    }
-    return exp2;
-}
+
 int main() {
-    int myVar = 1;
-    int z = 1;
-    int x = 1;
-    printf("請輸入一個整數");
-    scanf_s("%d", &myVar);
-    printf("你總共要執行幾項\?");
-    scanf_s("%d", &z);
-    printf("第三題exp的次方值\?");
-    scanf_s("%d", &x);
-    printf("%d", fact(myVar));
-    printf("%f", exponen(z));
-    printf("%f", exponen_x(z, x));
+    int SIZE[10];
+
+    // 輸入學生學號
+    printf("請輸入10位學生學號：\n");
+    for (int i = 0; i < 10; i++) {
+        scanf_s("%d", &SIZE[i]);
+    }
+
+    // 排序
+    bubbleSort(SIZE, 10);
+
+    // 印出排序後的結果
+    printf("排序後的結果：\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", SIZE[i]);
+    }
+    printf("\n");
+
+    // 輸入欲查找的值
+    int key;
+    printf("請輸入欲查找的值：\n");
+    scanf_s("%d", &key);
+
+    // 二元搜尋
+    int result = binarySearch(SIZE, 0, 9, key);
+
+    // 印出結果
+    if (result != -1) {
+        printf("找到值 %d 在陣列中的位置：%d\n", key, result);
+    }
+    else {
+        printf("找不到\n");
+    }
+
     return 0;
 }
