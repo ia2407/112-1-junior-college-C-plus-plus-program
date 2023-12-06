6175db2def25601cd497f86bc2c9daf01859e4ac
ghyisjj55555 <109556902+ghyisjj55555@users.noreply.github.com>
2023-12-06 15:45:25 +0800

    1206

---
+++ 1111134020/Program10/Program10/Program10.cpp
@@ -0,0 +1,109 @@
+﻿// Program10.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#define SIZE 10
+
+// function main begins program execution
+void bubble(int array[SIZE]);
+size_t binarySearch(const int b[], int searchKey, size_t low, size_t high);
+int main(void)
+{
+    // initialize a
+    int a[SIZE], key = 1, result;
+    puts("Data items in original order");
+    // output original array
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("請輸入第%d個整數", i + 1);
+        scanf_s("%d", &a[i]);
+    }
+    bubble(a);
+    puts("\nData items in ascending order");
+    // output sorted array
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+    puts("");
+
+    while (key != 0)
+    {
+        printf("\n請輸入你要搜尋的整數");
+        scanf_s("%d", &key);
+        result = binarySearch(a, key, 0, SIZE - 1);
+        if (result != -1)
+            printf("你要找的%d在陣列a[]中第%d個元素", key, result);
+        else
+        {
+            printf("你要找的%d在陣列a[]中不存在", key);
+        }
+
+    }
+}
+
+
+
+// function to perform binary search of an array
+size_t binarySearch(const int b[], int searchKey, size_t low, size_t high)
+{
+    // loop until low index is greater than high index
+    while (low <= high) {
+
+        // determine middle element of subarray being searched
+        size_t middle = (low + high) / 2;
+
+        // display subarray used in this loop iteration
+
+
+        // if searchKey matched middle element, return middle
+        if (searchKey == b[middle]) {
+            return middle;
+        }
+
+        // if searchKey is less than middle element, set new high
+        else if (searchKey < b[middle]) {
+            high = middle - 1; // search low end of array      
+        }
+
+        // if searchKey is greater than middle element, set new low
+        else {
+            low = middle + 1; // search high end of array        
+        }
+    } // end while
+
+    return -1; // searchKey not found
+}
+
+
+
+
+
+void bubble(int array[SIZE])
+{
+    unsigned int pass;
+    // bubble sort                                         
+    // loop to control number of passes                    
+    for (unsigned int pass = 1; pass < SIZE; ++pass) {
+
+        // loop to control number of comparisons per pass   
+        for (size_t i = 0; i < SIZE - 1; ++i) {
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (array[i] > array[i + 1]) {
+                int hold = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = hold;
+            }
+        }
+    }
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後
