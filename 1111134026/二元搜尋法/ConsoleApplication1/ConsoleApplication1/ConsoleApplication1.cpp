6e7fdb1313254005ab1902fa698f354a42d466b2
Alann999 <144888962+Alann999@users.noreply.github.com>
2023-12-06 15:54:19 +0800

    12/06

---
+++ 1111134026/二元搜尋法/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,91 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#define SIZE 10
+
+// function main begins program execution
+void bubble(int array[SIZE]);
+int binarySearch(const int b[], int searchKey, int low, int high);
+int main(void)
+{
+    // initialize a
+    int a[SIZE], key = 1, result;
+    puts("Data items in original order");
+    // output original array
+    for (int i = 0; i < SIZE; ++i) {
+        printf("請輸入第%d個整數", i + 1);
+        scanf_s("%d", &a[i]);
+    }
+    bubble(a);
+    puts("\nData items in ascending order");
+    // output sorted array
+    for (int i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+    puts("");
+
+    while (key != 0) 
+    {
+        printf("\n請輸入你要搜尋的值");
+        scanf_s("%d", &key);
+            result = binarySearch(a,key,0,SIZE-1);
+        if (result != -1)
+            printf("你要找的%d在陣列a[]中第%d個元素!",key, result);
+        else
+            printf("你要找%d不在陣列a[]!",key);
+    }
+}
+
+int binarySearch(const int b[], int searchKey,int low,int high)
+{
+    // loop until low index is greater than high index
+    while (low <= high) {
+
+        // determine middle element of subarray being searched
+       int middle = (low + high) / 2;
+        // if searchKey matched middle element, return middle
+        
+        
+            if (searchKey == b[middle])
+            {
+                return middle;
+            }
+
+            // if searchKey is less than middle element, set new high
+            else if (searchKey < b[middle])
+            {
+                high = middle - 1; // search low end of array      
+            }
+
+            // if searchKey is greater than middle element, set new low
+            else
+            {
+                low = middle + 1; // search high end of array        
+            }
+        } // end while
+
+        return -1; // searchKey not found
+ }
+
+
+
+void bubble(int array[SIZE])
+{
+    unsigned int pass;
+    // bubble sort                                         
+    // loop to control number of passes                    
+    for (unsigned int pass = 1; pass < SIZE; ++pass) {
+
+        // loop to control number of comparisons per pass   
+        for (size_t i = 0; i < SIZE - 1; ++i) {
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (array[i] > array[i + 1]) {
+                int hold = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = hold;
+            }
+        }
+    }
+}
