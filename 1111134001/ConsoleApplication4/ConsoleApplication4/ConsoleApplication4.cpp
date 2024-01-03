7d90a13f712964aecb7fd32a5f5d4faa166f8050
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-01-03 15:42:57 +0800

    Add files via upload

--- 1111134001/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ 1111134001/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,68 +1,67 @@
 ﻿#include <stdio.h>
 #include <string.h>
-#define SIZE 10   //學生人數
-#define EXAM 4      //每個學生的資料、成績
-
+#define SIZE 10
+#define EXAM 4 
+    struct student {  //結構定義
+        char name[5];
+        int ID, math, compu;
+        float avg;
+    };
 // function main begins program execution
-void bubble(int array[][EXAM], const char* name[SIZE],int (*fptr)(int a,int b));
-int INC(int a, int b);
-int DEC(int a, int b);
-void swap(int* d1ptr, int* d2ptr);
+void bubble(struct student myStu[SIZE]);//定義
+
 int main(void)
 {
+    char inputName[20];
+    typedef int myInt;
+    myInt i;
+    student myStu[SIZE], tempStu, * prtStu;//結構宣告
+    prtStu = myStu;
     // initialize a
     int studentGrade[SIZE][EXAM] = { {1,74,55,0},{2,56,88,0}, {3,84,11,0},
     {4,88,52,0}, {5,100,100,0}, {6,0,0,0}, {7,51,81,0}, {8,74,14,0},
     {9,77,55,0}, {10,97,68,0} };
-    int i, chose=0 ;
-    //int a[SIZE] = { 96, 64, 45, 81, 100, 12, 89, 68, 77, 47 };
-    printf("請輸入名字\n");
-    scanf_s("%d");
-    const char* name[SIZE] = { };
-    //puts("Data items in original order");
+    const char* name[SIZE] = { "Danny","Jerry","Tom","Sue","Mary","John","Mark","Nina","Apple","Juice" };
+    puts("Data items in original order");
     // output original array
-    for (i = 0; i < SIZE; ++i) {//計算平均
-        studentGrade[i][3] = (studentGrade[i][1] + studentGrade[i][2]) / 2;
+   
+    for (i = 0; i < SIZE; ++i) {   //結構存入資料
+        printf("\n請輸入第%d筆學生資料", i+1);
+        printf("\n請輸入學生姓名");
+        scanf_s("%s", inputName,5);
+        strcpy_s(myStu[i].name,inputName);
+        printf("\n請輸入學生學號");
+        scanf_s("%d",&myStu[i].ID);
+        printf("\n請輸入學生成績");
+        scanf_s("%d", &myStu[i].math);
+        printf("\n請輸入計概成績");
+        scanf_s("%d", &myStu[i].compu);
+        myStu[i].avg = ((float)myStu[i].math+myStu[i].compu)/2;
     }
 
     printf("%10s  %5s %5s %5s %5s\n", "Name", "ID", "Math", "Compu", "Avg");
 
-    for (i = 0; i < SIZE; ++i) {
-        printf("%10s  %5d %5d %5d %5d", name[i], studentGrade[i][0], studentGrade[i][1], studentGrade[i][2], studentGrade[i][3]);
+    for (i = 0; i < SIZE; ++i) {    //印出結構內容
+        printf("%10s  %5d %5d %5d %5f", myStu[i].name, myStu[i].ID, myStu[i].math, myStu[i].compu, myStu[i].avg);
         printf("\n");
     }
-    printf("\n請告訴我您要遞增(1)還是遞減(2)排序?");
-    scanf_s("%d", &chose);
+    printf("\n");
 
-    if (chose == 1)
-        bubble(studentGrade, name,INC);
-    else if (chose == 2)
-        bubble(studentGrade, name,DEC);
+    bubble(myStu);
 
     puts("\nData items in ascending order");
     printf("%10s  %5s %5s %5s %5s\n", "Name", "ID", "Math", "Compu", "Avg");
 
     for (i = 0; i < SIZE; ++i) {
-        printf("%10s  %5d %5d %5d %5d", name[i], studentGrade[i][0], studentGrade[i][1], studentGrade[i][2], studentGrade[i][3]);
+        printf("%10s  %5d %5d %5d %5f", myStu[i].name, myStu[i].ID, myStu[i].math, myStu[i].compu, myStu[i].avg);
         printf("\n");
     }
-    puts("");
+    puts("");     
 }
 
-
-int INC(int a, int b)
+void bubble(struct student myStu[SIZE])
 {
-    return a > b;
-}
-
-int DEC(int a, int b)
-{
-    return a > b;
-}
-
-void bubble(int array[][EXAM], const char* name[SIZE],int(*fptr)(int a, int b))
-{
-    const char* temp;
+    struct student temp;
     unsigned int pass, i;
     // bubble sort                                         
     // loop to control number of passes                    
@@ -72,21 +71,11 @@ void bubble(int array[][EXAM], const char* name[SIZE],int(*fptr)(int a, int b))
         for (i = 0; i < SIZE - 1; ++i) {
             // compare adjacent elements and swap them if first 
             // element is greater than second element           
-            if ((*fptr)(array[i][3] , array[i + 1][3])) {
-                swap(&array[i][0], &array[i + 1][0]);
-                swap(&array[i][1], &array[i + 1][1]);
-                swap(&array[i][2], &array[i + 1][2]);
-                swap(&array[i][3], &array[i + 1][3]);
-                temp = name[i];
-                name[i] = name[i + 1];
-                name[i + 1] = temp;
+            if (myStu[i].avg < myStu[i + 1].avg) {
+    temp=myStu[i];
+    myStu[i]=myStu[i+1];
+    myStu[i+1]=temp;
             }
         }
     }
-}
-void swap(int* d1ptr, int* d2ptr)
-{
-    int hold = *d1ptr;
-    *d1ptr = *d2ptr;
-    *d2ptr = hold;
 }
